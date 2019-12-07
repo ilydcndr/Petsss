3ea5b731c0c8680fa53822576bae9b26f22662d7
ilydcndr <ilaydacundar@hotmail.com>
2019-12-07 14:31:49 +0200

    Updates

---
+++ precache-manifest.54dcde0d110b3b5935e624170200b4f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b127ece29f97c7913a01984b1275d525",
+    "url": "/petsss/index.html"
+  },
+  {
+    "revision": "1e82c43cf7f11abbec60",
+    "url": "/petsss/static/css/2.7aa6f31f.chunk.css"
+  },
+  {
+    "revision": "a1abbd7a8134410d1306",
+    "url": "/petsss/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "1e82c43cf7f11abbec60",
+    "url": "/petsss/static/js/2.45ad0bb6.chunk.js"
+  },
+  {
+    "revision": "a1abbd7a8134410d1306",
+    "url": "/petsss/static/js/main.4a26b871.chunk.js"
+  },
+  {
+    "revision": "1297c5fca44c09269d45",
+    "url": "/petsss/static/js/runtime-main.cbf26974.js"
+  }
+]);
